9ad1381df6ace31ddfc27474bfa60f2606dda283
Nicholas <nicholas.stevenson@protonmail.com>
2018-05-24 22:08:37 -0700

    first commit

---
+++ app/src/main/java/nl/breadfish/breadfish/GameActivity.java
@@ -0,0 +1,12 @@
+package nl.breadfish.breadfish;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GameActivity extends AppActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+}
