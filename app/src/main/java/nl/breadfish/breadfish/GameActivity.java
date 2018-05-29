8beb158e72a0661c99816020b84779525e4c78aa
Nicholas <nicholas.stevenson18@auhsdschools.org>
2018-05-29 09:30:59 -0700

    Created and instantiated GameView, MainThread, and fixed errors

--- app/src/main/java/nl/breadfish/breadfish/GameActivity.java
+++ app/src/main/java/nl/breadfish/breadfish/GameActivity.java
@@ -1,12 +1,16 @@
 package nl.breadfish.breadfish;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class GameActivity extends AppCompatActivity {
+public class GameActivity extends Activity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(new GameView(this));
     }
 }
--- app/src/main/java/nl/breadfish/breadfish/MainActivity.java
+++ app/src/main/java/nl/breadfish/breadfish/MainActivity.java
@@ -1,5 +1,6 @@
 package nl.breadfish.breadfish;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -20,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                new GameActivity();
+                startActivity(new Intent(MainActivity.this, GameActivity.class));
             }
         });
     }
